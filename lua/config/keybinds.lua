9e851e9af5c015daccc046b6d0fa072aa77a0739
Dezlost <dezt.root@gmail.com>
2026-09-04 08:37:44 -0400

    Feat(vim-tmux,keybinds): add new plugins

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Nagevacion
+
+vim.keymap.set('n', '<c-h>', function() vim.cmd.wincmd('h') end)
+vim.keymap.set('n', '<c-j>', function() vim.cmd.wincmd('j') end)
+vim.keymap.set('n', '<c-k>', function() vim.cmd.wincmd('k') end)
+vim.keymap.set('n', '<c-l>', function() vim.cmd.wincmd('l') end)
